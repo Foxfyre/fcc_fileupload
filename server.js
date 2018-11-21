e30d24c34c5f2e5d154ab5184f6a1c1512f32ccf
Glitch (winter-name) <none>
2018-11-21 20:34:54 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+const multer = require("multer");
+const upload = multer({ dest: 'uploads/' })
+// require and use "multer"...
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res, next) {
+  console.log(req);
+  res.json({
+    filename: req.file.originalname,
+    size: req.file.size + " bytes"
+  });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
